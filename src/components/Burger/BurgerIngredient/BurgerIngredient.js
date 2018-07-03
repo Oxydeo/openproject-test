0324a684af416f539e548f10846c390879790c6a
Oxydeo <vincent.fournier95@outlook.com>
2018-07-03 18:47:25 +0200

    first rendering of the burger without dynamic content of it :)

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './BurgerIngredient.css';
+import PropTypes from 'prop-types';
 
-const burgerIngredient = (props) => {
+class BurgerIngredient extends Component {
+  render () {
   let ingredient = null;
 
-  switch (props.type) {
+  switch (this.props.type) {
     case ('bread-bottom'):
       ingredient = <div className={classes.BreadBottom}></div>;
       break;
@@ -31,7 +33,13 @@ const burgerIngredient = (props) => {
     default: 
       ingredient = null;
   }
+
   return ingredient;
+  }
+}
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
 };
 
 export default BurgerIngredient;
