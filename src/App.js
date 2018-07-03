50ae985325e9049050b1c6ed0e249863e313ff0b
Oxydeo <vincent.fournier95@outlook.com>
2018-07-03 16:27:44 +0200

    ajout d'un component layout et auxe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi!</h1>
-        <p>I'm a react app</p>
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>  
       </div>
     );
   }
